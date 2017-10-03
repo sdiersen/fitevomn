2a9bec5d0f99bf564f6158f4488b5c6e2986c73b
Steve Diersen <srdiersen@gmail.com>
2017-10-03 15:00:39 -0500

    need to figure out custom breakpoints for boostrap containers

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -26,6 +26,36 @@ $(document).ready(function() {
 		$(loc + ' li span').html(id);
 
 	});
+	var topoffset = 50;
+	$('.loader').fadeOut(1000);
+
+	$('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+		var hash = $(this).find('li.active a').attr('href');
+		if (hash !== '#slideshow') {
+			$('header nav').addClass('inbody');
+		} else {
+			$('header nav').removeClass('inbody');
+		}
+	});
+
+	$('body').scrollspy({
+		target: 'header .navbar',
+		offset: topoffset
+	});
+
+	//Use smooth scrolling when clicking on navigation
+	$('.navbar a').click(function() {
+		if(location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if(target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top-topoffset+2 }, 500);
+				return false;
+			}
+		}
+	});
+	
 });
 
 
