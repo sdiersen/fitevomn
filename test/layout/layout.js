4b43e9ffb12c71bffb52ee29eb09e2e3028f2aab
Steve Diersen <srdiersen@gmail.com>
2017-11-25 07:46:43 -0600

    working layout 11 15

--- test/layout/layout.js
+++ test/layout/layout.js
@@ -2,18 +2,26 @@ $(document).ready(function() {
 	var events = $(".myCarouselItem").toArray();
 	var number_of_events = events.length;
 	var current_event = getCurrentEvent(events);
-
+	$("#testvalue").html("hello");
 	$("#myCarouselActiveItem").html(current_event);
+	$("div.myCarouselNextItem").click(function() {
+		$(this).css({"background-color":"black"});
+	});
 
 	var counter = 0;
 });
 function getCurrentEvent(events) {
-	$.each(events, function(key, value) {
-		if(value.match("myCarouselActiveItem")) {
-			return key;
-		}
-	});
-	return 0;
+	// $.each(events, function(key, value) {
+	// 	var blah = value;
+	// 	// if(value.indexOf("myCarouselActiveItem") >= 0) {
+
+	// 	// 	return key;
+	// 	// }
+	// 	$("div.myCarouselPrev").append(blah);
+	// });
+	// return 53;
+	var data = $(":contains('myCarouselActiveItem')").attr("data-carousel-item-number");
+	return data;
 }
 function setNextEvent(event) {
 
