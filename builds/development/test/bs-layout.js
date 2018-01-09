60dc9a8733e709724006d85652d3cda848b6044a
Steve Diersen <srdiersen@gmail.com>
2018-01-09 17:14:54 -0600

    bs-layout

--- builds/development/test/bs-layout.js
+++ builds/development/test/bs-layout.js
@@ -1,26 +1,29 @@
 $(document).ready(function() {
 	var width = $(window).width();
 
-	changePics();
+	// changePics();
 
-	$(window).resize(function() {
-		width = $(window).width();
-		changePics();
-	});
+	// $(window).resize(function() {
+	// 	width = $(window).width();
+	// 	changePics();
+	// });
 	
 
-	function changePics() {
+	// function changePics() {
+	// 	$("#banner1").attr("src","../images/banner/test-banner-1a.jpg");
+	// 	$("#banner2").attr("src","../images/banner/test-banner-2a.jpg");
+	// 		$("#banner3").attr("src","../images/banner/test-banner-3a.jpg");
 
-		if (width < 1344) {
-			$("#banner1").attr("src","../images/banner/test-banner-1a.jpg");
-			$("#banner2").attr("src","../images/banner/test-banner-2a.jpg");
-			$("#banner3").attr("src","../images/banner/test-banner-3a.jpg");
+		// if (width < 1344) {
+		// 	$("#banner1").attr("src","../images/banner/test-banner-1a.jpg");
+		// 	$("#banner2").attr("src","../images/banner/test-banner-2a.jpg");
+		// 	$("#banner3").attr("src","../images/banner/test-banner-3a.jpg");
 
-		} else {
-			$("#banner1").attr("src","../images/banner/test-banner-1b.jpg");
-			$("#banner2").attr("src","../images/banner/test-banner-2b.jpg");
-			$("#banner3").attr("src","../images/banner/test-banner-3b.jpg");
-		}
+		// } else {
+		// 	$("#banner1").attr("src","../images/banner/test-banner-1b.jpg");
+		// 	$("#banner2").attr("src","../images/banner/test-banner-2b.jpg");
+		// 	$("#banner3").attr("src","../images/banner/test-banner-3b.jpg");
+		// }
 	}
 
 	// $("a.dropdown-toggle").hover(function ()
